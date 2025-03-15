bce788a29928600f2fc5aa82319cea0c84e70e2e
jaycel <jhunchesterlalongisip@gmail.com>
2025-03-15 05:47:46 +0000

    wip: slu launch and cd cmd

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
 
-
-const SLU_RL_BUFFSIZE = 1024;
+#define SLU_RL_BUFFSIZE 1024
 
 char *slu_read_line() {
   // initialize buffer size
@@ -24,8 +27,8 @@ char *slu_read_line() {
     c = getchar();
     
     // check if end of file if it is return null else return the char
-    if(c == EOF || c == "\n") {
-      buffer[position] = "\0";
+    if(c == EOF || c == '\n') {
+      buffer[position] = '\0';
       return buffer;
     }else {
       buffer[position] = c;
@@ -45,12 +48,13 @@ char *slu_read_line() {
   }
 }
 
-const SLU_TOK_BUFFSIZE = 64;
-const SLU_TOK_DELIM = "\t\r\n\a";
+#define SLU_TOK_BUFFSIZE 64
+#define SLU_TOK_DELIM "\t\r\n\a"
 
 char **slu_split_line(char *line) {
   // declare bufsize
-  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  int bufsize = SLU_TOK_BUFFSIZE;
+  int position = 0;
   // allocate mem for **tokens [], [], []
   char **tokens = malloc(bufsize * sizeof(char*));
   // pointer variable to hold token
@@ -58,11 +62,11 @@ char **slu_split_line(char *line) {
   
   // display error mem allocation
   if (!tokens) {
-    fprintf(stderr, "shellyu: Mem allocation error \n);
+    fprintf(stderr, "shellyu: Mem allocation error \n");
     exit(EXIT_FAILURE);
   }
   // tokenize words by spaces base on the delim and return pointer to the next token found in the string
-  token = strtok(line, LSH_TOK_DELIM);
+  token = strtok(line, SLU_TOK_DELIM);
   while (token != NULL) {
     // assign each token pointer to tokens [token], [token], [token]
     tokens[position] = token;
@@ -71,16 +75,16 @@ char **slu_split_line(char *line) {
 
     // realloc mem if not enough
     if (position >= bufsize) {
-      bufsize += LSH_TOK_BUFSIZE;
+      bufsize += SLU_TOK_BUFFSIZE;
       tokens = realloc(tokens, bufsize * sizeof(char*));
       // display error mem alloc
       if (!tokens) {
-        fprintf(stderr, "shellyu: Mem allocation error \n);
+        fprintf(stderr, "shellyu: Mem allocation error \n");
         exit(EXIT_FAILURE);
       }
     }
     // to rerun strtok and remember it state
-    token = strtok(NULL, LSH_TOK_DELIM);
+    token = strtok(NULL, SLU_TOK_DELIM);
   }
   // and put NULL on the rest of the mem space
   tokens[position] = NULL;
@@ -90,7 +94,7 @@ char **slu_split_line(char *line) {
 
 int slu_launch(char **args) {
   // variable for process id
-  pid_t, pid, wpid;
+  pid_t pid, wpid;
   // variable that holds status
   int status;
   
@@ -111,7 +115,7 @@ int slu_launch(char **args) {
     do {
       wpid = waitpid(pid, &status, WUNTRACED);
       // loop until change of status either exit or other status codes
-    } while (!WIFEXCITED(status) && !WIFSIGNALED(status));
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
   }
   return 1;
 }
@@ -130,10 +134,10 @@ char *builtin_str[] = {
 
 // assign each function into the corresponding builtin cmd
 int (*builtin_func[]) (char **) = {
-  &slu_cd;
-  &slu_help;
-  &slu_exit;
-}
+  &slu_cd,
+  &slu_help,
+  &slu_exit
+};
 
 // get the number of built in cmd
 int slu_num_builtins() {
@@ -161,7 +165,7 @@ int slu_help(char **args) {
   printf("Type program names and arguments, and hit enter \n");
   printf("The following are built in: \n");
 
-  for (i = 0; i < slu_sum_builtins(); i++) {
+  for (i = 0; i < slu_num_builtins(); i++) {
       printf(" %s\n", builtin_str[i]);
   }
   printf("Use the man command for information on other programs. \n");
@@ -171,6 +175,21 @@ int slu_help(char **args) {
 // exit cmd function
 int slu_exit(char **args) {
   return 0;
+}
+
+int slu_execute(char **args) {
+  int i;
+  if (args[0] == NULL) {
+    return 1;
+  }
+  // looop through each cmd and comparing if it exists in the args pos 0
+  for (i = 0; i < slu_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+  // else launch the program
+  return slu_launch(args);
 }
 
 void slu_loop(){
